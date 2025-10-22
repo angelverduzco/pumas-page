b41af2ef2736f254d91afea92b001cde791953b7
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-10-22 13:52:43 -0600

    Added scope attribute to table columns and rows

--- src/components/Campeonatos.jsx
+++ src/components/Campeonatos.jsx
@@ -7,44 +7,44 @@ export default function Campeonatos() {
       <table>
         <thead>
           <tr>
-            <th>Temporada</th>
-            <th>Entrenador</th>
-            <th>Final vs.</th>
+            <th scope="col">Temporada</th>
+            <th scope="col">Entrenador</th>
+            <th scope="col">Final vs.</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>1976-1977</td>
+            <th scope="row">1976-1977</th>
             <td>Jorge Marik</td>
             <td>Leones Negros</td>
           </tr>
           <tr>
-            <td>1980-1981</td>
+            <th scope="row">1980-1981</th>
             <td>Velibor Milutinović</td>
             <td>Cruz Azul</td>
           </tr>
           <tr>
-            <td>1990-1991</td>
+            <th scope="row">1990-1991</th>
             <td>Miguel Mejía Barón</td>
             <td>América</td>
           </tr>
           <tr>
-            <td>Clausura 2004</td>
+            <th scope="row">Clausura 2004</th>
             <td>Hugo Sánchez</td>
             <td>Chivas</td>
           </tr>
           <tr>
-            <td>Apertura 2004</td>
+            <th scope="row">Apertura 2004</th>
             <td>Hugo Sánchez</td>
             <td>Monterrey</td>
           </tr>
           <tr>
-            <td>Clausura 2009</td>
+            <th scope="row">Clausura 2009</th>
             <td>Ricardo Ferretti</td>
             <td>Pachuca</td>
           </tr>
           <tr>
-            <td>Clausura 2011</td>
+            <th scope="row">Clausura 2011</th>
             <td>Guillermo Vázquez</td>
             <td>Morelia</td>
           </tr>
