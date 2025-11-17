292f5d9a74b72c2b0e41d926b675e1f3eafebc4a
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-11-16 18:35:51 -0600

    Swapped position of title and img in Estadio section

--- src/components/Estadio/index.jsx
+++ src/components/Estadio/index.jsx
@@ -4,9 +4,9 @@ import "./Estadio.css";
 export default function Estadio() {
   return (
     <section className="estadio">
-      <h2>Estadio Olímpico Universitario</h2>
       <figure>
         <img src={estadioImg} alt="" />
+        <h2>Estadio Olímpico Universitario</h2>
         <figcaption>
           Capacidad: 72,000 espectadores | Inaugurado: 1952
         </figcaption>
